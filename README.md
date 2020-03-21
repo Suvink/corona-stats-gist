85011be6deb849b3e94111da79883aa11cb164e1
Suvink <hello@suvink.me>
2020-03-21 21:27:30 +0530

    Final refactors and readme included

--- README.md
+++ README.md
@@ -1,34 +1,37 @@
-<p align="center">
-  <img width="400" src="https://user-images.githubusercontent.com/4658208/54479268-752bb580-47f1-11e9-9d01-edd8b574aa92.png">
-  <h3 align="center">bird-box</h3>
-  <p align="center">Update a pinned gist to contain the latest tweets of a user</p>
-</p>
+
+<center>
+   <img  width="600"  src="https://i.imgur.com/BfvMCb6.png">
+   <h3  align="center">Corona-Stats-box</h3>
+   <h6>By Suvink </h6>
+   <p  align="center">🦠 Update a gist to show Coronavirus stats </p>
+</center>
 
 ---
-
-> 📌✨ For more pinned-gist projects like this one, check out: https://github.com/matchai/awesome-pinned-gists
 
 ## Setup
 
 ### Prep work
 
 1. Create a new public GitHub Gist (https://gist.github.com/)
-1. Create an access token with the `gist` scope and copy it. (https://github.com/settings/tokens/new)
-1. Create a new Twitter app (https://developer.twitter.com/en/apps/create)
-1. On the App page for your newly created app, generate an "Access token & access token secret" and copy all keys and tokens.
+
+2. Create an access token with the `gist` scope and copy it. (https://github.com/settings/tokens/new)
+
+3. Edit the environment variables in .github/workflows/schedule.yml:
+      - GIST_ID: The ID portion from your gist url: `https://gist.github.com/matchai/6d5f84419863089a167387da62dd7081`.
+      
 
 ### Project setup
 
+
 1. Fork this repo
-1. Edit the [environment variables](https://github.com/matchai/bird-box/blob/bcc05c5376710b231c9a81c102df6e51efcc6fc7/.github/workflows/schedule.yml#L13-L19) in `.github/workflows/schedule.yml`:
 
-   - **TWITTER_USER:** The user handle of the twitter account.
-   - **GIST_ID:** The ID portion from your gist url: `https://gist.github.com/matchai/`**`6d5f84419863089a167387da62dd7081`**.
+2. Go to the repo **Settings > Secrets**
 
-1. Go to the repo **Settings > Secrets**
-1. Add the following environment variables:
-   - **GH_TOKEN:** The GitHub access token generated above.
-   - **TWITTER_CONSUMER_KEY:** Your Twitter consumer API key.
-   - **TWITTER_CONSUMER_SECRET:** Your Twitter consumer secret.
-   - **TWITTER_ACCESS_TOKEN_KEY:** Your Twitter access token key.
-   - **TWITTER_ACCESS_TOKEN_SECRET:** Your Twitter access token secret.
+3. Add the following environment variables:
+
+-  **GH_TOKEN:** The GitHub access token generated above.
+
+-  **GIST_ID:** The ID portion from your gist url: `https://gist.github.com/Suvink/`**`6d5f84419863089a167387da62dd7081`**
+
+4. Use Github Actions to automate the updates. Copy the contents in `.github/workflows/schedule.yaml` file and paste in a new Github Actions Workflow.
+##### Based on joshghent's Rescue-Box! Happy Coding!
--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 require("dotenv").config();
 const Octokit = require("@octokit/rest");
 const request = require('request-promise');
-const moment = require('moment');
-const changeCase = require('change-case')
 
 const {
   GIST_ID: gistId,
-  TWITTER_USER: twitterHandle,
-  TWITTER_CONSUMER_KEY: consumerKey,
-  TWITTER_CONSUMER_SECRET: consumerSecret,
-  TWITTER_ACCESS_TOKEN_KEY: accessTokenKey,
-  TWITTER_ACCESS_TOKEN_SECRET: accessTokenSecret,
   GH_TOKEN: githubToken
 } = process.env;
-
 
 const octokit = new Octokit({
   auth: `token ${githubToken}`
@@ -26,11 +18,10 @@ async function main () {
   })
  
   await updateGist(stats)
-  console.log(stats);
 }
 
 async function updateGist(stats) {
-  
+
   let gist;
   try {
     gist = await octokit.gists.get({ gist_id: gistId });
@@ -38,44 +29,58 @@ async function updateGist(stats) {
     console.error(`Unable to get gist\n${error}`);
   }
 
-
   const lines = []
 
-  lines.push([`😷 Stats for 2020-03-21 10:00:21`])
+  lines.push([`🦠 Stats for `+ stats.data.update_date_time])
 
   const line1 = [
-    '🌎 Global Cases'.padEnd(19),
-    '275864'.padEnd(10),
-    'Global Deaths'.padEnd(19),
-    '275864'.padEnd(10)
+    '🌎 Global Cases:'.padEnd(19),
+    String(stats.data.global_total_cases).padEnd(10),
+    '💀 Global Deaths:'.padEnd(23),
+    String(stats.data.global_deaths).padEnd(10)
   ];
   lines.push(line1.join(" "));
 
   const line2 = [
-    'Global Cases'.padEnd(19),
-    '275864'.padEnd(10),
-    'Global Deaths'.padEnd(19),
-    '275864'.padEnd(10)
+    '🇱🇰 Local Cases:'.padEnd(21),
+    String(stats.data.local_total_cases).padEnd(10),
+    '🇱🇰 Local Hospitalized:'.padEnd(25),
+    String(stats.data.local_total_number_of_individuals_in_hospitals).padEnd(10)
   ];
   lines.push(line2.join(" "));
 
+
+  let globalrec = Math.floor((stats.data.global_recovered/stats.data.global_total_cases)*100);
   const line3 = [
-    '🇱🇰 Local Cases'.padEnd(19),
-    '275864'.padEnd(10),
-    'Global Deaths'.padEnd(19),
-    '275864'.padEnd(10)
+    "🌎 Global Recovery: ".padEnd(19),
+    generateBarChart(globalrec, 20),
+    String(globalrec+"%").padEnd(10)
   ];
   lines.push(line3.join(" "));
 
+  let localrec = Math.floor((stats.data.local_recovered/stats.data.local_total_cases)*100);
   const line4 = [
-    'Global Cases'.padEnd(19),
-    '275864'.padEnd(10),
-    'Global Deaths'.padEnd(19),
-    '275864'.padEnd(10)
+    "🇱🇰 Local Recovery: ".padEnd(22),
+    generateBarChart(localrec, 20),
+    String(localrec+"%").padEnd(10)
   ];
   lines.push(line4.join(" "));
 
 
+  function generateBarChart(percent, size) {
+    const syms = "░▏▎▍▌▋▊▉█";
+  
+    const frac = (size * 8 * percent) / 100;
+    const barsFull = Math.floor(frac / 8);
+    const semi = frac % 8;
+    const barsEmpty = percent === 100 ? 0 : size - barsFull - 1;
+  
+    return [
+      syms.substring(8, 9).repeat(barsFull),
+      syms.substring(semi, semi + 1),
+      syms.substring(0, 1).repeat(barsEmpty)
+    ].join("");
+  }
 
   // Get original filename to update that same file
   const filename = Object.keys(gist.data.files)[0];
